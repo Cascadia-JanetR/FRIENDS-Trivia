35b00dd02bea3492a85fc11b2bf453c5a27f2c2f
Ben Harrison <benjaminpharrison@gmail.com>
2013-11-27 02:20:50 -0800

    Added a play_game activity and its layout
    The play_game activity will be where the user answers 5 questions. Just
    started on the layout (added a background pic, but need to add a
    textview and buttons).

--- app/gen/edu/cascadia/friendsfantrivia/R.java
+++ app/gen/edu/cascadia/friendsfantrivia/R.java
@@ -27,19 +27,25 @@ public final class R {
         public static final int question_bg_01=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int blankView01=0x7f080001;
-        public static final int blankView02=0x7f080003;
-        public static final int playButton=0x7f080002;
+        public static final int action_settings=0x7f080008;
+        public static final int blankView01=0x7f080005;
+        public static final int blankView02=0x7f080007;
+        public static final int imageView1=0x7f080003;
+        public static final int playButton=0x7f080006;
+        public static final int playGameLayout=0x7f080001;
+        public static final int questionBottomRelativeLayout=0x7f080004;
+        public static final int questionTopRelativeLayout=0x7f080002;
         public static final int startButton=0x7f080000;
     }
     public static final class layout {
         public static final int activity_level_start=0x7f030000;
-        public static final int activity_start=0x7f030001;
+        public static final int activity_play_game=0x7f030001;
+        public static final int activity_start=0x7f030002;
     }
     public static final class menu {
         public static final int level_start=0x7f070000;
-        public static final int start=0x7f070001;
+        public static final int play_game=0x7f070001;
+        public static final int start=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -53,6 +59,7 @@ public final class R {
         public static final int start_level_three=0x7f050005;
         public static final int start_level_two=0x7f050004;
         public static final int title_activity_level_start=0x7f050009;
+        public static final int title_activity_play_game=0x7f05000b;
     }
     public static final class style {
         /** 
---
+++ app/src/edu/cascadia/friendsfantrivia/PlayGameActivity.java
@@ -0,0 +1,41 @@
+/*
+ * PlayGameActivity.java
+ * 
+ * Activity where the user answers 5 questions.
+ */
+
+package edu.cascadia.friendsfantrivia;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PlayGameActivity extends Activity {
+	
+	// Instance variables
+	int curLevel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_play_game);
+		
+		// Get the current level from the intent
+		// curLevel will be -1 if there's a problem
+		curLevel = getIntent().getIntExtra("curLevel", -1);
+		// Test toast - make sure we got the correct current level
+		//Toast.makeText(this, Integer.toString(curLevel), Toast.LENGTH_SHORT).show();
+		
+		// Put curLevel as the title in the action bar
+		setTitle("Level " + curLevel);
+	}
+
+//  No menu for right now
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.play_game, menu);
+//		return true;
+//	}
+
+}
