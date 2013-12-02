a8a3d931f21293f088e31ded0e083e7c043c811d
Cascadia-JanetR <jrasque@hotmailcom>
2013-12-02 05:29:16 -0800

    Added images to code

--- app/gen/edu/cascadia/friendsfantrivia/R.java
+++ app/gen/edu/cascadia/friendsfantrivia/R.java
@@ -31,6 +31,9 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
         public static final int ic_not_answered=0x7f020003;
         public static final int question_bg_01=0x7f020004;
+        public static final int app_icon=0x7f020000;
+        public static final int front=0x7f020001;
+        public static final int ic_app=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090013;
