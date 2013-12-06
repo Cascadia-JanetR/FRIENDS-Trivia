d3a101f0bda4bcc16daac25138f05cb483ccd511
henryngoy <Henryngoy@yahoo.com>
2013-12-06 14:00:04 -0800

    Sync

--- app/src/edu/cascadia/friendsfantrivia/GameOverActivity.java
+++ app/src/edu/cascadia/friendsfantrivia/GameOverActivity.java
@@ -196,5 +196,5 @@ public class GameOverActivity extends Activity {
  * 
  * 
  * Cascadia Community College - BIT 272 (Mobile App Development) - Fall 2013
- * Ben Harrison, Janet Rasque, Henry NGoy
+ * Ben Harrison, Janet Rasque, Henry Ngoy
  */
