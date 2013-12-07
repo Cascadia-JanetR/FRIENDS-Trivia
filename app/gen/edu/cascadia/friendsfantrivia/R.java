4be21440110234c02e87a3181ee93669af3850fa
Ben Harrison <benjaminpharrison@gmail.com>
2013-12-06 17:13:15 -0800

    No changes...

--- app/gen/edu/cascadia/friendsfantrivia/R.java
+++ app/gen/edu/cascadia/friendsfantrivia/R.java
@@ -120,7 +120,8 @@ public final class R {
     }
     public static final class raw {
         public static final int friends_theme=0x7f050000;
-        public static final int janiceohmy=0x7f050001;
+        public static final int howdoing=0x7f050001;
+        public static final int janiceohmy=0x7f050002;
     }
     public static final class string {
         public static final int action_settings=0x7f080001;
